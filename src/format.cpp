e908556a414eea6b966e5fdd081a037425a57561
Shing-Yan Loo <yan99033@gmail.com>
2021-06-09 18:55:18 -0600

    style: change some wording---not important

--- src/format.cpp
+++ src/format.cpp
@@ -19,9 +19,10 @@ string Format::ToStrWithLeadingZero(long num) {
 // REMOVE: [[maybe_unused]] once you define the function
 // Bonus: if HH exceeds 99, a day counter is prepended (e.g., if elapsed time is
 // 108:00:00, then the visualization becomes '4 days, 12:00:00'. ), following
-// the htop format (not implemented because the processes use the same format)
+// the htop format 
 string Format::ElapsedTime(long seconds) {
-  long dd, hh, mm, ss;
+  long hh, mm, ss;
+  // long dd;
   std::string dd_str = "";
 
   hh = floor(seconds / SECS_IN_HR);
@@ -32,6 +33,7 @@ string Format::ElapsedTime(long seconds) {
 
   ss = seconds;
 
+  // (not implemented because the processes use the same format)
   // if (hh > 99) {
   //   dd = floor(hh / 24);
   //   hh -= dd * 24;
